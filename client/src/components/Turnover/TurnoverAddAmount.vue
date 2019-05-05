65d05e2d4cc1b69649aa89d2a4198822708233c5
matbz <bentz.matthias@gmail.com>
2019-05-05 16:05:53 +0200

    shared

--- client/src/components/Turnover/TurnoverAddAmount.vue
+++ client/src/components/Turnover/TurnoverAddAmount.vue
@@ -5,7 +5,7 @@
         <button class="button-cancel" >Cancel</button>
       </div>
       <div class="pure-u-1-3 header-text">
-        Add Turnover ({{ budgetId }})
+        Add Turnover ( Budget: {{ budgetId }})
       </div>
       <div class="pure-u-1-3 header-logout">
         <button v-if="!aTurnover.id" class="button-logout" @click="switchBudget()">Switch</button>
